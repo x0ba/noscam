0045422047c5cc61dcfd0e887e587b2793ad70f2
Daniel Xu <hi@danielx.me>
2026-07-04 17:24:41 -0700

    add tailwind + shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite-plus";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   staged: {
     "*": "vp check --fix",
   },
   fmt: {},
+  plugins: [tailwindcss()],
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
     rules: { "vite-plus/prefer-vite-plus-imports": "error" },
